8b5d9d3f80b047925aea8baf325ad3a9e5f7fcbb
el1521 <122532754+el1521@users.noreply.github.com>
2023-11-08 00:19:17 +0000

    Update game.py
    新加了Minon.restart() 下一步考虑剪掉些没用的行

--- game.py
+++ game.py
@@ -2,18 +2,22 @@ import itertools
 import numpy as np
 import pandas as pd
 
+
 class Minon:
     def __init__(self, attack: int, health: int):
+        self._attack = attack
+        self._health = health
+
         self.attack = attack
         self.health = health
         self.death = False
-        self.reborn = False
-    
+        self.reborn = 0
+
     def set_death(self):
         self.attack = 0
         self.health = 0
         self.death = True
-    
+
     def battle(self, other):
         if self.death or other.death:
             return False
@@ -29,82 +33,83 @@ class Minon:
                     minon.set_death()
         return True
 
+    def restart(self):
+        self.attack = self._attack
+        self.health = self._health
+        self.death = False
+        self.reborn = 0
+
     def __repr__(self) -> str:
         return f"({self.attack},{self.health})"
+
 
 class Game:
     def __init__(self):
         self.minons_A = []
         self.minons_B = []
-    
+
     def set_minons_A(self, minons: list):
         self.minons_A = [Minon(m[0], m[1]) for m in minons]
 
     def set_minons_B(self, minons: list):
         self.minons_B = [Minon(m[0], m[1]) for m in minons]
-    
+
     def get_power(self):
         power = 0
         for minon in self.minons_A:
-            #power += minon.health + minon.attack
+            power += minon.health + minon.attack
             power += minon.attack
         for minon in self.minons_B:
-            #power -= minon.health + minon.attack
+            power -= minon.health + minon.attack
             power -= minon.attack
         return power
+
+    def restart(self):
+        for m in self.minons_A + self.minons_B:
+            m.restart()
 
     def __repr__(self) -> str:
         return f"Player A: {self.minons_A} \n Player B: {self.minons_B}"
 
 
+def generate_strats(G: Game):
+    m = len(G.minons_A)
+    n = len(G.minons_B)
+    A_strats = []
+    for A_idx in itertools.permutations(list(range(m))):
+        for B_idx in itertools.product(
+             *tuple([list(range(-1, n)) for _ in range(m)])):
+            A_strats.append(list(zip(A_idx, B_idx)))
+
+    B_strats = [_ for _ in range(n)]
+    return A_strats, B_strats
+
+
 def payoff(G: Game, A_strat: list, B_strat: int):
-    G.minons_B[B_strat].reborn = True
+    G.restart()
+    G.minons_B[B_strat].reborn = 1
     for a in A_strat:
         if a[1] != -1:
             minon_A = G.minons_A[a[0]]
             minon_B = G.minons_B[a[1]]
             if not minon_A.battle(minon_B):
                 return -100
-    return G.get_power()
+    power = G.get_power()
+    return power
 
 
-def generate_strats(m, n):
-    A_strats = []
-    for A_idx in itertools.permutations(list(range(m))):
-        for B_idx in itertools.product(*tuple([list(range(-1, n)) for _ in range(m)])):
-            A_strat = []
-            for a in A_idx:
-                for b in B_idx:
-                    A_strat.append((a, b))
-            A_strats.append(A_strat)
-
-    B_strats = []
-    for i in range(n):
-        bs = [0 for _ in range(n)]
-        bs[i] = 1
-        B_strats.append(bs)
-    return A_strats, B_strats
-
-
-def payoff_table(G, As, Bs):
+def payoff_table(G):
+    As, Bs = generate_strats(G)
     data = np.zeros((len(As), len(Bs)))
     for i in range(len(As)):
         for j in range(len(Bs)):
-            data[i][j] = payoff(G,As[i],Bs[j])
-    return data
-
-
+            data[i][j] = payoff(G, As[i], Bs[j])
+    df = pd.DataFrame(data, index=[str(A) for A in As])
+    return df
 
 
 G = Game()
-G.set_minons_A([(2, 3), (1, 4)])
-G.set_minons_B([(2, 3), (1, 4)])
+G.set_minons_A([(2, 3), (2, 3)])
+G.set_minons_B([(2, 3), (2, 3)])
 
-m = 2
-n = 2
-
-
-'''
-df = pd.DataFrame(data=data, dtype=np.int8, index=[str(i) for i in a_strat_list])
-print(df)
-'''
+print(payoff_table(G))
